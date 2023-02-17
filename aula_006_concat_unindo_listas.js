721f35185b34ad0544d723178a3b6df6f2aef28f
wagner <wagner.santos@accurate.com.br>
2023-02-17 08:11:48 -0300

    Exemplo concat

---
+++ aula_006_concat_unindo_listas.js
@@ -0,0 +1,6 @@
+const salaJS = ["Evaldo", "Camis", "Mari"]
+const salaPython = ["Ju", "Leo", "Raquel"]
+
+// Como concat nao altera o array, cria-se um novo
+const salasUnificadas = salaJS.concat(salaPython)
+console.log(salasUnificadas)
