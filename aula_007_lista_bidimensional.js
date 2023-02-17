a5f1257fd50a99096889d18fbb8251e00b780576
wagner <wagner.santos@accurate.com.br>
2023-02-17 09:24:58 -0300

    Exemplo lista bidimensional

---
+++ aula_007_lista_bidimensional.js
@@ -0,0 +1,9 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"]
+const medias = [10, 8, 7.5, 9]
+
+const alunosEMedias = [alunos, medias]
+console.log(alunosEMedias)
+
+console.log(`
+A aluna na posição 1 da lista de alunos é a ${alunosEMedias[0][1]} e sua média é ${alunosEMedias[1][1]}.
+`)
