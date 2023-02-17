09106665095fc0311283f01baa7ca779cf1dfdeb
wagner <wagner.santos@accurate.com.br>
2023-02-17 09:25:39 -0300

    Exemplos com concat

--- aula_006_concat_unindo_listas.js
+++ aula_006_concat_unindo_listas.js
@@ -4,3 +4,16 @@ const salaPython = ["Ju", "Leo", "Raquel"]
 // Como concat nao altera o array, cria-se um novo
 const salasUnificadas = salaJS.concat(salaPython)
 console.log(salasUnificadas)
+
+
+const arrayOriginal = ["Maria", "Carlos", "Eduardo", "Samanta"]
+const arrayConcat = arrayOriginal.concat(["André", "Fernanda"], ["Ricardo", "Ana"], ["Marcelo", "Bia"])
+
+console.log(arrayConcat)
+console.log(arrayOriginal)
+
+const arrayOriginalNumeros = [50, 60, 70]
+const arrayConcatNumeros = arrayOriginalNumeros.concat([80, [90, 100]])
+
+console.log(arrayConcatNumeros)
+console.log(arrayOriginalNumeros)
